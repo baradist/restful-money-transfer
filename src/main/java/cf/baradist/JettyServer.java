f208bcc58223f673a2ce2e0084168f2af8e942ad
Oleg Grigoryev <oleg_grigoryev@epam.com>
2018-05-08 14:44:34 +0300

    ServletContextHandler with test entry point

--- src/main/java/cf/baradist/JettyServer.java
+++ src/main/java/cf/baradist/JettyServer.java
@@ -1,12 +1,31 @@
 package cf.baradist;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class JettyServer {
     public static void main(String[] args) throws Exception {
-        final Server server = new Server(8080);
-        server.setHandler(new HelloHandler());
-        server.start();
-        server.join();
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+
+        Server jettyServer = new Server(8080);
+        jettyServer.setHandler(context);
+
+        ServletHolder jerseyServlet = context.addServlet(
+                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+        jerseyServlet.setInitOrder(0);
+
+        // Tells the Jersey Servlet which REST service/class to load.
+        jerseyServlet.setInitParameter(
+                "jersey.config.server.provider.classnames",
+                EntryPoint.class.getCanonicalName());
+
+        try {
+            jettyServer.start();
+            jettyServer.join();
+        } finally {
+            jettyServer.destroy();
+        }
     }
 }
