33eb73b11b3eb5112241e8516fecb7ea25e68154
Oleg Grigoryev <baradist@ya.ru>
2018-05-09 21:49:57 +0300

    + User

---
+++ src/main/java/cf/baradist/model/User.java
@@ -0,0 +1,50 @@
+package cf.baradist.model;
+
+import java.util.Objects;
+
+public final class User {
+    private final long id;
+    private final String name;
+    private final String email;
+
+    public User(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
