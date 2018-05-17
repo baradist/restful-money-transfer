5f9575acebbf32b2a98cbc4a3398354586edb877
Oleg Grigoryev <oleg_grigoryev@epam.com>
2018-05-17 16:29:12 +0300

    swagger-ui static content moved; JettyServer.java reconfigured

--- src/main/java/cf/baradist/JettyServer.java
+++ src/main/java/cf/baradist/JettyServer.java
@@ -2,6 +2,7 @@ package cf.baradist;
 
 import io.swagger.jaxrs.config.DefaultJaxrsConfig;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.server.ServerProperties;
@@ -20,15 +21,12 @@ public class JettyServer {
     }
 
     private static void runServer() throws Exception {
+        Server jettyServer = new Server(8080);
+
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
 
-        Server jettyServer = new Server(Integer.parseInt(Configurer.getProperties().getProperty(DATA_REST_PORT, "80")));
-        jettyServer.setHandler(context);
-
-        // Setup API resources
-        ServletHolder apiServlet = context.addServlet(ServletContainer.class,
-                Configurer.getProperties().getProperty(DATA_REST_BASE_PATH, "/api/*"));
+        ServletHolder apiServlet = context.addServlet(ServletContainer.class, "/api/*");
         apiServlet.setInitOrder(1);
         apiServlet.setInitParameter(ServerProperties.PROVIDER_PACKAGES,
                 "cf.baradist.controller;io.swagger.jaxrs.json;io.swagger.jaxrs.listing");
@@ -37,6 +35,15 @@ public class JettyServer {
         ServletHolder swaggerServlet = context.addServlet(DefaultJaxrsConfig.class, "/swagger-core");
         swaggerServlet.setInitOrder(2);
         swaggerServlet.setInitParameter("api.version", "1.0.0");
+
+        // Lastly, the default servlet for root content (always needed, to satisfy servlet spec)
+        // It is important that this is last.
+        ServletHolder holderPwd = new ServletHolder("default", new DefaultServlet());
+        holderPwd.setInitParameter("resourceBase", "./src/main/resources/webapp/");
+        holderPwd.setInitParameter("dirAllowed", "true");
+        context.addServlet(holderPwd, "/*");
+
+        jettyServer.setHandler(context);
 
         try {
             jettyServer.start();
